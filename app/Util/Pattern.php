ece59d67cc2c81874b8b90fc63e3d531862fa848
wesleymartinss <wesley.smartins@outlook.com>
2020-07-07 01:09:04 -0300

    Modify access UUIDPatern

--- app/Util/Pattern.php
+++ app/Util/Pattern.php
@@ -7,7 +7,7 @@ class Pattern {
     protected static $UUIDv4Pattern = '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i';
 
     public static function verifyValidUUID($receivedUUID){
-        if(preg_match($UUIDv4Pattern, $receivedUUID)){
+        if(preg_match(Pattern::$UUIDv4Pattern, $receivedUUID)){
             return true;
         }
         return false;
